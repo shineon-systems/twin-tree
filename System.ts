482150ed627515ea4fc5597a6dfa0b66cbc88c1f
Sebastien Ringrose <seb@codeworks.me>
2023-01-19 09:11:13 +0000

    feat: connected state

--- System.ts
+++ System.ts
@@ -1,9 +1,31 @@
+type CustomEventListener = (e: CustomEvent) => unknown
+
 export default class System extends EventTarget {
+  // constructed once then deserialised from storage
+  // so consistent uuid is hard-coded into device
   id = crypto.randomUUID()
+  connected = false
   children: System[] = []
 
   constructor(public name: string, public parent?: System) {
     super()
+  }
+
+  connect(id: string | string[]) {
+    if (Array.isArray(id)) id.forEach(id => this.connect(id))
+    else if (this.id === id) this.connected = true
+    else this.children.forEach(child => child.connect(id))
+  }
+
+  disconnect(id: string) {
+    if (this.id === id) this.connected = false
+    else this.children.forEach(child => child.disconnect(id))
+  }
+
+  checkConnect() {
+    if (!this.connected) {
+      throw new Error(`System with name ${this.name} and id ${this.id} is not connected`)
+    }
   }
 
   addChild(name: string) {
@@ -12,12 +34,16 @@ export default class System extends EventTarget {
     return child
   }
 
-  addListener(type: string, listener: (e: CustomEvent) => void) {
-    this.addEventListener(type, listener as (e: Event) => void)
-    if (this.parent) this.parent.addListener(type, listener)
+  dispatchEvent(e: CustomEvent) {
+    this.checkConnect()
+    return EventTarget.prototype.dispatchEvent.call(this, e)
   }
 
-  dispatch(e: CustomEvent) {
-    this.dispatchEvent(e)
+  addEventListener(type: string, listener: CustomEventListener | { handleEvent: CustomEventListener } | null) {
+    if (!listener) throw new Error(`Listener function not provided for event with ${type} on system with name ${this.name} and id ${this.id}`)
+    if (typeof listener !== "function") listener = listener.handleEvent
+
+    if (this.parent) this.parent.addEventListener(type, listener)
+    return EventTarget.prototype.addEventListener.call(this, type, listener as EventListener)
   }
 }
--- System_test.ts
+++ System_test.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.161.0/testing/asserts.ts";
+import { assert, assertThrows } from "https://deno.land/std@0.161.0/testing/asserts.ts";
 import System from "./System.ts";
 
 Deno.test("System", async (t) => {
@@ -6,26 +6,43 @@ Deno.test("System", async (t) => {
   // and 3 "row" sub-systems that each have 4 "plant" sub-systems
   const hydroponic_wall = new System("hydro_root")
   const rows = [
+    "row_0",
     "row_1",
     "row_2",
-    "row_3",
   ].map(name => hydroponic_wall.addChild(name))
   const plants = [
+    "plant_0",
     "plant_1",
     "plant_2",
-    "plant_3",
-    "plant_4"
+    "plant_3"
   ].flatMap(name => rows.map(row => row.addChild(name)))
 
-  await t.step("Sub-systems correctly added", () => {
+  await t.step("System and sub-systems linked", () => {
     assert(plants.length === 12)
     assert(plants[0] === hydroponic_wall.children[0].children[0])
   })
 
-  await t.step("Sub-systems correctly receive events from parent systems", () => {
+  await t.step("Cannot dispatch events before connection", () => {
+    let ran = false
+    rows[0].addEventListener("bad-event", () => ran = true)
+    assertThrows(() => rows[0].dispatchEvent(new CustomEvent("bad-event", { detail: "how can I exist when the system isn't connected?" })))
+    assert(!ran)
+
+    hydroponic_wall.connect(hydroponic_wall.id)
+    hydroponic_wall.dispatchEvent(new CustomEvent("bad-event"))
+
+    // should this be false? i.e. do we not run listeners from parents events
+    // if the node that the listener was added to is not connected? 
+    // seeing as listeners are intended to be control actions, probably... 
+    assert(ran)
+  })
+
+  await t.step("Sub-systems connect and receive events from ancestors", () => {
     let event: CustomEvent | undefined
-    plants[0].addListener("temperature", (e) => event = e)
-    hydroponic_wall.dispatch(new CustomEvent("temperature", { detail: 27 }))
+
+    hydroponic_wall.connect([rows[0].id, plants[0].id])
+    plants[0].addEventListener("temperature", (e) => event = e)
+    hydroponic_wall.dispatchEvent(new CustomEvent("temperature", { detail: 27 }))
 
     assert(event)
     assert(event.target === hydroponic_wall)
